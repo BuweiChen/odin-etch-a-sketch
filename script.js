fc6faecb1d3b9fcaa57952e7dca7ba4508e01b98
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-01-13 11:26:15 -0600

    Add resize function and button to do so

--- script.js
+++ script.js
@@ -20,3 +20,35 @@ for (let i = 0; i < 4; i++) {
         gridColumn.appendChild(gridPixel);
     }
 }
+
+let resizeButton = document.querySelector("#resize-button");
+resizeButton.addEventListener("click", () => {
+    let newWidth = Number(prompt("input new width of grid"));
+    if (newWidth === NaN || newWidth <= 0) {
+        alert("invalid width, please try again");
+        return;
+    }
+    resetGrid(newWidth);
+});
+
+function resetGrid(width) {
+    document.body.removeChild(sketchGrid);
+    sketchGrid = document.createElement("div");
+    sketchGrid.setAttribute("class", "grid")
+    document.body.appendChild(sketchGrid);
+    for (let i = 0; i < width; i++) {
+        let gridColumn = document.createElement("div");
+        gridColumn.setAttribute("class", "grid-column");
+        sketchGrid.appendChild(gridColumn);
+        for (let j = 0; j < width; j++) {
+            let gridPixel = document.createElement("div");
+            gridPixel.setAttribute("class", "grid-pixel");
+            gridPixel.addEventListener("mouseenter", () => {
+                if (state_mouseDown) {
+                    gridPixel.classList.add("grid-pixel-drawn");
+                }
+            })
+            gridColumn.appendChild(gridPixel);
+        }
+    }
+}
