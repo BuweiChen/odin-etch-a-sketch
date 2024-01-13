d2bd039928da6aeceff568c596472ceb01cb9af6
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-01-13 16:03:43 -0600

    Fix bug where rgb doesn't work with progressive

--- script.js
+++ script.js
@@ -69,10 +69,25 @@ function resetGrid(width) {
             gridPixel.setAttribute("class", "grid-pixel");
             gridPixel.addEventListener("mouseenter", () => {
                 if (state_mouseDown) {
-                    gridPixel.classList.add("grid-pixel-drawn");
+                    let red = 255, green = 0, blue = 0, a = 1;
                     if (state_rgb) {
-                        gridPixel.style.backgroundColor = `rgb(${Math.round(Math.random() * 255)},${Math.round(Math.random() * 255)},${Math.round(Math.random() * 255)})`;
+                        red = Math.round(Math.random() * 255);
+                        green = Math.round(Math.random() * 255);
+                        blue = Math.round(Math.random() * 255);
                     }
+                    if (state_progressive) {
+                        if (!gridPixel.hasAttribute("data-opacity")) {
+                            gridPixel.setAttribute("data-opacity", "0.1");
+                        }
+                        else if (gridPixel.getAttribute("data-opacity") !== "1") {
+                            gridPixel.setAttribute("data-opacity", String(Number(gridPixel.getAttribute("data-opacity")) + 0.1));
+                        }
+                        a = gridPixel.getAttribute("data-opacity");
+                    }
+                    else {
+                        gridPixel.removeAttribute("data-opacity");
+                    }
+                    gridPixel.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${a})`;
                 }
             })
             gridColumn.appendChild(gridPixel);
